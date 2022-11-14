e8804c9ec6fdb185ab5ebe3773cb79cbcf2f0c72
Jason Fong <jasonfng7@gmail.com>
2022-11-14 18:16:23 -0500

    final commit on week7

--- app/src/main/java/fongjason/lab01/ChatRoomViewModel.java
+++ app/src/main/java/fongjason/lab01/ChatRoomViewModel.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 
 public class ChatRoomViewModel extends ViewModel {
+
     public MutableLiveData<ArrayList<ChatMessage>> messages = new MutableLiveData<>();
 
 }
